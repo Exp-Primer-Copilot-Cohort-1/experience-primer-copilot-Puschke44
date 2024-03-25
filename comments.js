0e087d50ab8a0e3baf7837c300948aef124033a7
Puschke44 <164519901+Puschke44@users.noreply.github.com>
2024-03-25 18:12:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,27 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
-});
-
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
-
-//Test
+ //Create web Server using Express 
+    const express = require('express');
+    const app = express();
+    const port = 3000;
+    const path = require('path');
+    const fs = require('fs');
+    const bodyParser = require('body-parser');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static('public'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    }
+    );
+    app.post('/comments', (req, res) => {
+        const comment = req.body.comment;
+        fs.appendFile('comments.txt', comment + '\n', (err) => {
+            if (err) {
+                res.status(500).send('Error');
+            } else {
+                res.status(200).send('Success');
+            }
+        });
+    });
+    app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+    });
